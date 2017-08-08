9d58032248676ed93d94c43b2c9184a06e8770c6
Mimi Ughetta <30319657+mimeyu3@users.noreply.github.com>
2017-08-08 09:31:27 -0400

    fix visited

--- ViewYork/src/pages/visited/visited.ts
+++ ViewYork/src/pages/visited/visited.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { OpeningPage } from '../OpeningPage/OpeningPage'
 
 @Component({
   selector: 'page-visited',
   templateUrl: 'visited.html'
 })
 export class visited {
-
+  items: any;
   constructor(public navCtrl: NavController) {
 
   }
-
+  showOpeningPage() {
+      this.navCtrl.push(OpeningPage);
+  }
 }
