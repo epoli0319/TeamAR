d4769d65e5fc3e844030ff476d49b5efb640ed66
Mimi Ughetta <30319657+mimeyu3@users.noreply.github.com>
2017-08-08 10:50:19 -0400

    info buttons

--- ViewYork/src/pages/Informationpage/Informationpage.ts
+++ ViewYork/src/pages/Informationpage/Informationpage.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {---} from '../--/--'
+import {OpeningPage} from '../OpeningPage/OpeningPage';
+import {Subject} from '../Subject/Subject';
+import {Artist} from '../Artist/Artist';
+import {Location} from '../Location/Location';
+import {Why} from '../Why/Why';
+import {FunFacts} from '../FunFacts/FunFacts';
+
 
 @Component({
   selector: 'page-Informationpage',
@@ -11,7 +17,22 @@ export class Informationpage {
   constructor(public navCtrl: NavController) {
 
   }
-  show--Page() {
-      this.navCtrl.push(--);
+  showOpeningPage() {
+      this.navCtrl.push(OpeningPage);
+  }
+  showSubjectPage() {
+      this.navCtrl.push(Subject);
+  }
+  showArtistPage() {
+      this.navCtrl.push(Artist);
+  }
+  showLocationPage() {
+      this.navCtrl.push(Location);
+  }
+  showWhyPage() {
+      this.navCtrl.push(Why);
+  }
+  showFunFactsPage() {
+      this.navCtrl.push(FunFacts);
   }
 }
