b00b225b1b5dd6885826d5a09f36b7f49603804a
Mimi Ughetta <30319657+mimeyu3@users.noreply.github.com>
2017-08-08 14:29:38 -0400

    camera

--- ViewYork/src/pages/OpeningPage/OpeningPage.ts
+++ ViewYork/src/pages/OpeningPage/OpeningPage.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {visited} from '../visited/visited'
-import {CameraPage} from '../CameraPage/CameraPage'
+import {Camera, CameraOptions} from '@ionic-native/camera';
 
 @Component({
   selector: 'page-OpeningPage',
   templateUrl: 'OpeningPage.html'
 })
 export class OpeningPage {
-  items: any;
-  constructor(public navCtrl: NavController) {
+items: any;
+public photo: any;
+public base64Image: string;
+constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
-    showvisited() {
-        this.navCtrl.push(visited);
-  }
-    showCameraPage() {
-        this.navCtrl.push(CameraPage);
+  showvisited() {
+      this.navCtrl.push(visited);
   }
 
+  ngOnInit(){
+    this.photo = []
+  }
+
+  takePhoto() {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photo.push(base64Image);
+      this.photo.reverse();
+      }, (err) => {
+      //Handle error
+      });
+  }
 }
