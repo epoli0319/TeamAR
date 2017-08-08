1696d81d6ff616842911f33372e279fc5228c9c2
mimeyu3 <30319657+mimeyu3@users.noreply.github.com>
2017-08-07 21:01:56 -0400

    Update OpeningPage.ts

--- ViewYork/src/pages/OpeningPage/OpeningPage.ts
+++ ViewYork/src/pages/OpeningPage/OpeningPage.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {visited} from '../visited/visited'
 
 @Component({
   selector: 'page-OpeningPage',
   templateUrl: 'OpeningPage.html'
 })
-export class HomePage {
-
+export class OpeningPage {
+  items: any;
   constructor(public navCtrl: NavController) {
 
   }
 
+  showVisitedPage() {
+      this.navCtrl.push(visited);
+  }
+  showCameraPage() {
+      this.navCtrl.push(Camera);
+  }
 }
