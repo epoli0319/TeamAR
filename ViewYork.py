c0919bf49943fc26fe7ebb21429432899f42ef37
epoli0319 <30319759+epoli0319@users.noreply.github.com>
2017-08-15 14:53:21 -0400

    Flask(newer)

--- ViewYork.py
+++ ViewYork.py
@@ -1,82 +1,50 @@
 ## To activate the virtual environment
 ## . venv/bin/activate
 
-
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy import Table
 from sqlalchemy import create_engine
 app = Flask(__name__)
-##db = SQLAlchemy(app)
 
+##****************************Database Connection******************************
+
+os.system('sudo /etc/init.d/mysql start') ## starts mysql database
 engine = create_engine('mysql+mysqldb://root:Aelin101$$@localhost/Statues')
 metadata= MetaData(bind=engine)
+Info = Table('Info', metadata,autoload=True)
 
-users = Table('Info', metadata, autoload=True)
-con = engine.connect()
-##metadata = MetaData()
-##metadata.reflect(bind=engine)
-##Session = orm.sessionmaker(bind=engine)
-##session = Session()
+##*****************************************************************************
 
 
-##class User(db.Model):
-##    __tablename__='Info'
-##    name= db.Column(db.VARCHAR(255), unique=True)
-##    subject = db.Column(db.VARCHAR(255), unique=True)
-##    artist = db.Column(db.VARCHAR(255), unique=True)
-##    location = db.Column(db.VARCHAR(255), unique=True)
-##    why = db.Column(db.VARCHAR(255), unique=True)
-##    funFacts = db.Column(db.VARCHAR(255), unique=True)
-##    def __init__(self, username, email):
-##        self.name = name
-##        self.subject = subject
-##        self.artist = artist
-##        self.location = location
-##        self.why = why
-##        self.funFacts = funFacts
-    
-##---------------##
-## Website Pages ##
-##---------------##
+
+def install_secret_key(app, filename='secret_key'):
+    filename = os.path.join(app.instance_path, filename)
+    try:
+        app.config['SECRET_KEY'] = open(filename, 'rb').read()
+    except IOError:
+        print 'Error: No secret key. Create it with:'
+        if not os.path.isdir(os.path.dirname(filename)):
+            print 'mkdir -p', os.path.dirname(filename)
+        print 'head -c 24 /dev/urandom >', filename
+        sys.exit(1)
+
+install_secret_key(app, filename='secret_key')
+
+
 @app.route('/')
 def home():
-    return render_template ('OpeningPage.html')
+    return "VIEW YORK"
 
-@app.route('/Artist/')
-def Artist():
-    return render_template ('Artist.html')
+@app.route('/Database/<info_type>')
+def Data(info_type):
+    result=engine.execute("SELECT * FROM Info WHERE name = 'Peter Cooper Statue'")
+    for row in result:
+        print row [0]
+    return "Info: " % info
+    
 
-@app.route('/Camera/')
-def Camera():
-    return render_template ('Camera.html')
-
-@app.route('/FunFacts/')
-def FunFacts():
-    return render_template ('FunFacts.html')
-
-@app.route('/InformationPage/')
-def InformationPage():
-    return render_template ('Informationpage.html')
-
-@app.route('/Location/')
-def Location():
-    return render_template ('Location.html')
-
-@app.route('/Subject/')
-def Subject():
-    return render_template ('Subject.html')
-
-@app.route('/Visited/')
-def Visited():
-    return render_template ('visited.html')
-
-@app.route('/Why/')
-def Why():
-    return render_template ('Why.html')
-
-@app.route('/Database/')
-def Database():
-    engine.execute('SELECT*FROM Info')
-    return 'Hello World'
+if __name__ == "__main__":
+	app.run(debug=True)
